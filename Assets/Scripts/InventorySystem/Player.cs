683b6adf1affc51dd3c02aca3ab2bbe301f13fd4
Oscar Devs <ohernandezba@gmail.com>
2024-07-19 17:20:21 +0200

    Items World Spawner

--- Assets/Scripts/InventorySystem/Player.cs
+++ Assets/Scripts/InventorySystem/Player.cs
@@ -13,9 +13,9 @@ public class Player : MonoBehaviour
         inventory = new Inventory();
         uiInventory.SetInventory(inventory);
 
-        ItemWorld.SpawnItemWorld(new Vector3(-0.4f, -2.82f), new Item { itemType = Item.ItemType.HealthPotion, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(-0.9f, -2.82f), new Item { itemType = Item.ItemType.ManaPotion, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(3, -2.82f), new Item { itemType = Item.ItemType.Sword, amount = 1 });
+        //ItemWorld.SpawnItemWorld(new Vector3(-0.4f, -2.82f), new Item { itemType = Item.ItemType.HealthPotion, amount = 1 });
+        //ItemWorld.SpawnItemWorld(new Vector3(-0.9f, -2.82f), new Item { itemType = Item.ItemType.ManaPotion, amount = 1 });
+        //ItemWorld.SpawnItemWorld(new Vector3(3, -2.82f), new Item { itemType = Item.ItemType.Sword, amount = 1 });
     }
 
     private void OnTriggerEnter2D(Collider2D other)
